3838c5e801b02b50bebbd169f91f25a687414738
Michael <52293899+MichaelDorner1502@users.noreply.github.com>
2022-01-11 21:34:23 +0100

    Revert "global update"
    This reverts commit 0394a5214e3dd955e94969e8f388583442e7a8cb.

--- src/global.js
+++ src/global.js
@@ -48,12 +48,10 @@ class Global extends React.Component {
 
     /*Getter*/
     getWindowHeight = () => {
-        this.setWindowHeight()
         return this.windowHeight
     }
 
     getWindowWidth = () => {
-        this.setWindowWidth
         return this.windowWidth
     }
     
@@ -158,4 +156,4 @@ var g = new Global(
 )
 
 /*global Variable*/
-//global.g = g;
+global.g = g;
