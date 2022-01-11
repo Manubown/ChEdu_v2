da4093eed0cefdb022670ae805c4d928a7108e8c
Michael <52293899+MichaelDorner1502@users.noreply.github.com>
2022-01-11 21:34:40 +0100

    Revert "Started global and Home"
    This reverts commit 642d78049bc5353f93a715fdb73437ae84e8b309.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,25 +1,12 @@
-/*React standart imports*/
-import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-/*global*/
-import global from "../global";
-
-export default class Home extends React.Component {
-    state = {
-        active: 0,
-        xTabLearnToPlay: 0, //x co-ordinate of tab one
-        xTabOnline: 0, //x co-ordinate of tab two
-        xTabChessBoard: 0, //x co-ordinate of tab three
-        translateX: new Animated.Value(0),
-        translateXTabLearnToPlay: new Animated.Value(0),
-        translateXTabOnline: new Animated.Value(width),
-        translateXTabChessBoard: new Animated.Value(width * 2),
-        translateXTabAnalysis: new Animated.Value(width * 3),
-        translateYLearnToPlay: -1000,
-        translateYOnline: -1000,
-        translateYChessboard: -1000,
-    };
-
+const testPage = () => {
+    return (
+      <View>
+        <Text>Testdd</Text>
+      </View>
+    );
     
-}
+  };
+
+  export default testPage;
