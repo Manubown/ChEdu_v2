79c28d2191eeddeaee9fa3779f1205e42b708766
Michael <52293899+MichaelDorner1502@users.noreply.github.com>
2022-01-11 22:23:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 /*Pages*/
-import Home from './pages/Home';
+import Home from './Pages/Home';
 
 const Stack = createNativeStackNavigator();
 
