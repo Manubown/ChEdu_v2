b55ae499255d291c410d80e45fcc4ec82089402c
Michael <52293899+MichaelDorner1502@users.noreply.github.com>
2022-02-18 10:53:21 +0100

    lol 0

--- src/Screens/CustomComponents/GameComponents.js
+++ src/Screens/CustomComponents/GameComponents.js
@@ -80,7 +80,7 @@ const renderElements = (pgnArray, nameArray, updateGamePGN, isClosed) => {
   console.log('Is CLosed:' + isClosed);
 
   //Set how many games get Showen
-  var ShowGames = 3;
+  var ShowGames = 0;
 
   var finalArray = [];
 
